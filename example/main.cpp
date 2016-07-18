4028835159ec7a33195afce4a288c3da927a17fe
tehokang <tehokang@gmail.com>
2016-07-18 15:07:06 +0900

    1.author: thkang2 2.project: anyserver 3.description:  - modified main example to use ServerApplication

--- example/main.cpp
+++ example/main.cpp
@@ -1,15 +1,81 @@
 #include "anyserver_controller.h"
+#include <unistd.h>
 
 using namespace anyserver;
 
+bool g_run = false;
+
+class ServerApplication : public IAnyServerControllerListener
+{
+public:
+    ServerApplication(int argc, char **argv)
+        : m_anyserver_controller(new AnyServerController(argv[1]))
+        , m_run(false)
+    {
+        m_anyserver_controller->setLogLevel(true, true, true, true);
+        m_anyserver_controller->setListener(this);
+    };
+
+    virtual ~ServerApplication()
+    {
+        if ( m_anyserver_controller )
+        {
+            delete m_anyserver_controller;
+            m_anyserver_controller = nullptr;
+        }
+    };
+
+    bool init()
+    {
+        return m_anyserver_controller->init();
+    }
+
+    bool start()
+    {
+        if ( m_anyserver_controller->start() )
+        {
+            m_run = true;
+            return true;
+        }
+        return false;
+    }
+
+    void stop()
+    {
+        m_anyserver_controller->stop();
+    }
+
+    virtual void onReceivedSystemSignal(int signal)
+    {
+        printf("received signal : %d \n", signal);
+        m_run = false;
+    }
+
+    bool isRun() { return m_run; };
+private:
+    bool m_run;
+
+    AnyServerController *m_anyserver_controller;
+};
+
+/**
+ * @brief Example server application
+ * @param argc
+ * @param argv
+ * @return
+ */
 int main(int argc, char **argv)
 {
-    AnyServerController anyserver(argv[1]);
-    anyserver.setLogLevel(true, true, true, true);
+    ServerApplication example(argc, argv);
 
-    if ( anyserver.init() && anyserver.start() )
+    if ( example.init() && example.start() )
     {
-        printf("Started AnyServerController \n");
+        while ( example.isRun() )
+        {
+            usleep(100000);
+        }
+        example.stop();
     }
+    printf("Stopped example \n");
     return 0;
 }

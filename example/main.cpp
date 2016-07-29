ebc636acd4555b37c65c51f0fcc8978cb8e75100
tehokang <tehokang@gmail.com>
2016-07-30 02:34:20 +0900

    Added handler of AnySererListener

--- example/main.cpp
+++ example/main.cpp
@@ -14,12 +14,38 @@ bool g_run = false;
  * @param argv
  * @return
  */
+
+class AnyServerListener : public IAnyServerListener
+{
+    virtual void onReceivedSystemSignal(int signal)
+    {
+        printf("[%s:%s:%d] \n", __FILE__, __FUNCTION__, __LINE__);
+    }
+
+    virtual void onClientConnected(size_t server_id, size_t client_id)
+    {
+        printf("[%s:%s:%d] \n", __FILE__, __FUNCTION__, __LINE__);
+    }
+
+    virtual void onClientDisconnected(size_t server_id, size_t client_id)
+    {
+        printf("[%s:%s:%d] \n", __FILE__, __FUNCTION__, __LINE__);
+    }
+
+    virtual void onReceive(size_t server_id, size_t client_id, char *msg, unsigned int msg_len)
+    {
+        printf("[%s:%s:%d] \n", __FILE__, __FUNCTION__, __LINE__);
+    }
+};
+
 int main(int argc, char **argv)
 {
     /**
      * @note AnyServer has to receive argv[1] that has configuration path
      */
     AnyServer server(argc, argv);
+    AnyServerListener listener;
+    server.addEventListener(&listener);
 
     if ( server.init() && server.start() )
     {

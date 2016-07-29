b0435ea45c0949b2f4bc608fb8af606ae396de59
tehokang <tehokang@gmail.com>
2016-07-29 17:46:54 +0900

    Fixed up renamed resources

--- example/main.cpp
+++ example/main.cpp
@@ -1,71 +1,12 @@
-#include "anyserver_controller.h"
+#include "anyserver.h"
 #include <unistd.h>
 #include <csignal>
-
+#include <stdio.h>
+#include <string>
+using namespace std;
 using namespace anyserver;
 
 bool g_run = false;
-
-class ServerApplication : public IAnyServerControllerListener
-{
-public:
-    ServerApplication(int argc, char **argv)
-        : m_anyserver_controller(new AnyServerController(argv[1]))
-        , m_run(false)
-    {
-        m_anyserver_controller->setLogLevel(true, true, true, true);
-        m_anyserver_controller->setListener(this);
-    };
-
-    virtual ~ServerApplication()
-    {
-        if ( m_anyserver_controller )
-        {
-            delete m_anyserver_controller;
-            m_anyserver_controller = nullptr;
-        }
-    };
-
-    bool init()
-    {
-        return m_anyserver_controller->init();
-    }
-
-    bool start()
-    {
-        if ( m_anyserver_controller->start() )
-        {
-            m_run = true;
-            return true;
-        }
-        return false;
-    }
-
-    void stop()
-    {
-        m_anyserver_controller->stop();
-    }
-
-    virtual void onReceivedSystemSignal(int signal)
-    {
-        switch ( signal )
-        {
-            case SIGPIPE:
-                printf("SIGPIPE will be ignored \n");
-                return;
-            default:
-                printf("received signal : %d \n", signal);
-                break;
-        }
-        m_run = false;
-    }
-
-    bool isRun() { return m_run; };
-private:
-    bool m_run;
-
-    AnyServerController *m_anyserver_controller;
-};
 
 /**
  * @brief Example server application
@@ -75,15 +16,15 @@ private:
  */
 int main(int argc, char **argv)
 {
-    ServerApplication example(argc, argv);
+    AnyServer server(argc, argv);
 
-    if ( example.init() && example.start() )
+    if ( server.init() && server.start() )
     {
-        while ( example.isRun() )
+        while ( server.isRun() )
         {
             usleep(100000);
         }
-        example.stop();
+        server.stop();
     }
     printf("Stopped example \n");
     return 0;
